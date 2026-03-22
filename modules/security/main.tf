984d26e66a3a5ab8c8e4cdcd920257f895035de7
Alex5418 <yiduow2@illinois.edu>
2026-03-21 19:37:26 -0500

    add port 3000 for grafana access

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -24,6 +24,14 @@ resource "aws_security_group" "k3s" {
     protocol    = "-1"
     self        = true
   }
+  
+  ingress {
+  description = "Grafana"
+  from_port   = 3000
+  to_port     = 3000
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 
   egress {
     from_port   = 0
